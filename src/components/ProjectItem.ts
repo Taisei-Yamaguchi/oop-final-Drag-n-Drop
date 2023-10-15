832949fe41cf06bb6fd0f594984931500f434f14
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2023-10-15 13:27:34 -0700

    Step12: create drag-drop.ts

--- src/components/ProjectItem.ts
+++ src/components/ProjectItem.ts
@@ -1,32 +1,51 @@
 import { Component } from "./base-component.js";
 import { Project } from "../models/project.js";
+import { Draggable } from "../helpers/drag-drop.js";
 
 //use this class in projectList
-export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
-    private title: string;
-    private people: number;
-    private description: string;
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable{
+    // private title: string;
+    // private people: number;
+    // private description: string;
+    private project: Project;
 
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, project.id);
-        this.title = project.title;
-        this.people = project.people;
-        this.description = project.description;
+        // this.title = project.title;
+        // this.people = project.people;
+        // this.description = project.description;
+        this.project=project;
         this.configure();
         this.renderContent();
     }
 
     configure() {
-        
+        // this.element.addEventListener('dragstart', this.dragStartHandler);
+        // this.element.addEventListener('dragend', this.dragEndHandler);
+
+        this.element.draggable = true; // Set the draggable attribute to true
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
     }
 
     renderContent() {
-        this.element.querySelector('h2')!.textContent = this.title;
+        this.element.querySelector('h2')!.textContent = this.project.title;
         this.element.querySelector('h3')!.textContent = this.members;
-        this.element.querySelector('p')!.textContent = this.description;
+        this.element.querySelector('p')!.textContent = this.project.description;
     }
 
     get members(): string {
-        return this.people === 1 ? '1 member assigned' : `${this.people} members assigned`;
+        return this.project.people === 1 ? '1 member assigned' : `${this.project.people} members assigned`;
+    }
+
+    dragStartHandler(event: DragEvent): void {
+        console.log('Drag Start');
+        event.dataTransfer!.setData('text/plain', this.project.id.toString());
+        event.dataTransfer!.effectAllowed = 'move';
+    }
+
+    dragEndHandler(event: DragEvent): void {
+        console.log('Drag End');
+        console.log(event)
     }
 }
