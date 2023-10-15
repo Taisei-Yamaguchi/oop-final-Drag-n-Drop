78f9baaa2bc112c0e8e4698ea3b55753c7b11360
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2023-10-15 15:53:39 -0700

    Step13: Implement DragTarget Interface.

--- src/models/project.ts
+++ src/models/project.ts
@@ -18,6 +18,11 @@ class Project {
         this.people = people;
         this.status = ProjectStatus.Active; // Initialize status as Active
     }
+
+    //switch Active and Finished
+    toggleStatus() {
+        this.status = this.status === ProjectStatus.Active ? ProjectStatus.Finished : ProjectStatus.Active;
+    }
 }
 
 export { Project, ProjectStatus };
